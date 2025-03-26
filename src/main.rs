8f255d086c66d5adad158a44f3f98e1bf754af57
Vivek Singh <vivek.singh.6967@gmail.com>
2025-03-26 22:09:07 +0530

    thread managment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use dotenvy::dotenv;
-use std::env;
 use std::io::BufRead;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
+use std::{env, thread};
 use std::{
     fs,
     io::{BufReader, Write},
-    net::{SocketAddr, TcpListener, TcpStream},
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -19,7 +22,23 @@ fn main() {
             for stream in listener.incoming() {
                 match stream {
                     Ok(stream) => {
-                        handle_connection(stream);
+                        let mut active_requests = Arc::new(Mutex::new(0));
+                        thread::spawn(move || {
+                            let active_requests = Arc::clone(&active_requests);
+                            {
+                                // scope for the lock
+                                let mut connection = active_requests.lock().unwrap();
+                                *connection += 1;
+                                // scope completition will automatically drop the lock
+                            }
+                            handle_connection(stream);
+                            {
+                                // scope for the lock
+                                let mut connection = active_requests.lock().unwrap();
+                                *connection -= 1;
+                                // scope completition will automatically drop the lock
+                            }
+                        });
                     }
                     Err(e) => {
                         println!("Failed to handle connection: {}", e);
@@ -35,12 +54,14 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let mut request_line  = buf_reader.lines().next();
-
-    let (status_line, file_name) = match request_line.unwrap().unwrap().as_str() {   
-        "GET / HTTP/1.1" => (Some("HTTP/1.1 200 OK"),Some("index.html")),
-        "GET /page1 HTTP/1.1" => (Some("HTTP/1.1 200 OK"),Some("page1.html")),
-        "GET /page2 HTTP/1.1" => (Some("HTTP/1.1 200 OK"),Some("page2.html")),
+    let mut request_line = buf_reader.lines().next();
+    let (status_line, file_name) = match request_line.unwrap().unwrap().as_str() {
+        "GET / HTTP/1.1" => (Some("HTTP/1.1 200 OK"), Some("index.html")),
+        "GET /page1 HTTP/1.1" => {
+            sleep(Duration::from_secs(10)); // Mimic a slow response
+            (Some("HTTP/1.1 200 OK"), Some("page1.html"))
+        }
+        "GET /page2 HTTP/1.1" => (Some("HTTP/1.1 200 OK"), Some("page2.html")),
         _ => (Some("HTTP/1.1 404 Not Found"), Some("404.html")),
     };
     // let response = "HTTP/1.1 200 OK\r\n";
@@ -48,7 +69,9 @@ fn handle_connection(mut stream: TcpStream) {
     let content_length = content.len();
     let response = format!(
         "{}Content-Length: {}\r\n\r\n{}",
-        status_line.unwrap(), content_length, content
+        status_line.unwrap(),
+        content_length,
+        content
     );
     stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
